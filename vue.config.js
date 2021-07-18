b2308068912e82dd4984c1296a434e27185d2197
Mochammad Fadholi <market.fadholi@gmail.com>
2021-07-18 13:02:11 +0700

    fix: set default host

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     devServer: {
-        port: 8000,
-        public: 'no-valid-link:8000',
+        host: 'localhost',
         https: {
             key: fs.readFileSync('./.ssl/cert/CA/localhost/localhost.decrypted.key'),
             cert: fs.readFileSync('./.ssl/cert/CA/localhost/localhost.crt')
